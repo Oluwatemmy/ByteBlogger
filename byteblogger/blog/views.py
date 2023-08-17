6b10e0e0919974dafd7e1eb934679c79153bda23
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-08-17 18:04:58 +0100

    Added SQLite Database to the Blog

--- byteblogger/blog/views.py
+++ byteblogger/blog/views.py
@@ -1,32 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2021'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2021'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'August 29, 2021'
-    }
-]
-
 def home(request):
     context= {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
