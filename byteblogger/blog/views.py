45a0031fb6f275cc34842cda8a6993661194d51a
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-08-17 16:08:07 +0100

    Added Main templates

--- byteblogger/blog/views.py
+++ byteblogger/blog/views.py
@@ -3,8 +3,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2021'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2021'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'August 29, 2021'
+    }
+]
+
 def home(request):
-    return HttpResponse("<h1>Byte Blogger Home Page</h1>")
+    context= {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Byte Blogger About Page</h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
